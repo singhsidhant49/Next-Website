6c61fdab16846dcf20d8b2a9ca14433c584ee268
siddhant singh <singhsidhant49@gmail.com>
2023-05-01 04:42:38 +0530

    second commit

--- sections/Contact.jsx
+++ sections/Contact.jsx
@@ -2,32 +2,32 @@ import React from 'react'
 
 const Contact = () => {
   return (
-//    <section class="relative flex flex-wrap lg:h-screen lg:items-center">
-//   <div class="w-full px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-8 lg:py-24">
-//     <div class="mx-auto max-w-lg text-center">
-//       <h1 class="text-2xl font-bold sm:text-3xl">Get started today!</h1>
+//    <section className="relative flex flex-wrap lg:h-screen lg:items-center">
+//   <div className="w-full px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-8 lg:py-24">
+//     <div className="mx-auto max-w-lg text-center">
+//       <h1 className="text-2xl font-bold sm:text-3xl">Get started today!</h1>
 
-//       <p class="mt-4 text-gray-500">
+//       <p className="mt-4 text-gray-500">
 //         Lorem ipsum dolor sit amet consectetur adipisicing elit. Et libero nulla
 //         eaque error neque ipsa culpa autem, at itaque nostrum!
 //       </p>
 //     </div>
 
-//     <form action="" class="mx-auto mb-0 mt-8 max-w-md space-y-4">
+//     <form action="" className="mx-auto mb-0 mt-8 max-w-md space-y-4">
 //       <div>
-//         <label for="email" class="sr-only">Email</label>
+//         <label for="email" className="sr-only">Email</label>
 
-//         <div class="relative">
+//         <div className="relative">
 //           <input
 //             type="email"
-//             class="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+//             className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
 //             placeholder="Enter email"
 //           />
 
-//           <span class="absolute inset-y-0 end-0 grid place-content-center px-4">
+//           <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
 //             <svg
 //               xmlns="http://www.w3.org/2000/svg"
-//               class="h-4 w-4 text-gray-400"
+//               className="h-4 w-4 text-gray-400"
 //               fill="none"
 //               viewBox="0 0 24 24"
 //               stroke="currentColor"
@@ -44,19 +44,19 @@ const Contact = () => {
 //       </div>
 
 //       <div>
-//         <label for="password" class="sr-only">Password</label>
+//         <label for="password" className="sr-only">Password</label>
 
-//         <div class="relative">
+//         <div className="relative">
 //           <input
 //             type="password"
-//             class="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+//             className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
 //             placeholder="Enter password"
 //           />
 
-//           <span class="absolute inset-y-0 end-0 grid place-content-center px-4">
+//           <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
 //             <svg
 //               xmlns="http://www.w3.org/2000/svg"
-//               class="h-4 w-4 text-gray-400"
+//               className="h-4 w-4 text-gray-400"
 //               fill="none"
 //               viewBox="0 0 24 24"
 //               stroke="currentColor"
@@ -78,15 +78,15 @@ const Contact = () => {
 //         </div>
 //       </div>
 
-//       <div class="flex items-center justify-between">
-//         <p class="text-sm text-gray-500">
+//       <div className="flex items-center justify-between">
+//         <p className="text-sm text-gray-500">
 //           No account?
-//           <a class="underline" href="">Sign up</a>
+//           <a className="underline" href="">Sign up</a>
 //         </p>
 
 //         <button
 //           type="submit"
-//           class="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
+//           className="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
 //         >
 //           Sign in
 //         </button>
@@ -94,11 +94,11 @@ const Contact = () => {
 //     </form>
 //   </div>
 
-//   <div class="relative h-64 w-full sm:h-96 lg:h-full lg:w-1/2">
+//   <div className="relative h-64 w-full sm:h-96 lg:h-full lg:w-1/2">
 //     <img
 //       alt="Welcome"
 //       src="https://images.unsplash.com/photo-1630450202872-e0829c9d6172?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-//       class="absolute inset-0 h-full w-full object-cover"
+//       className="absolute inset-0 h-full w-full object-cover"
 //     />
 //   </div>
 // </section>
@@ -108,39 +108,39 @@ const Contact = () => {
 
 <>
 
-{/* <section class="text-gray-600 body-font relative">
-  <div class="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
-    <div class="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-      <iframe width="100%" height="100%" class="absolute inset-0" frameborder="0" title="map" marginheight="0" marginwidth="0" scrolling="no" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=%C4%B0zmir+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" ></iframe>
+{/* <section className="text-gray-600 body-font relative">
+  <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+    <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+      <iframe width="100%" height="100%" className="absolute inset-0" frameborder="0" title="map" marginheight="0" marginwidth="0" scrolling="no" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=%C4%B0zmir+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" ></iframe>
      
     </div>
-    <div class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-      <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
-      <p class="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
-      <div class="relative mb-4">
-        <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
-        <input type="text" id="name" name="name" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+    <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+      <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
+      <p className="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
+      <div className="relative mb-4">
+        <label for="name" className="leading-7 text-sm text-gray-600">Name</label>
+        <input type="text" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
       </div>
-      <div class="relative mb-4">
-        <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
-        <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+      <div className="relative mb-4">
+        <label for="email" className="leading-7 text-sm text-gray-600">Email</label>
+        <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
       </div>
-      <div class="relative mb-4">
-        <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
-        <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+      <div className="relative mb-4">
+        <label for="message" className="leading-7 text-sm text-gray-600">Message</label>
+        <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
       </div>
-      <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-      <p class="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
+      <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+      <p className="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
     </div>
   </div>
 </section> */}
 
 
 
-<section class="text-gray-600 body-font relative">
+<section className="text-gray-600 body-font relative">
 
     
-  <div class="absolute inset-0 bg-gray-300">
+  <div className="absolute inset-0 bg-gray-300">
    
 
     <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d13780.80393093548!2d78.060078755603!3d30.288339499304236!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1682778841417!5m2!1sen!2sin" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="map" scrolling="no"  style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
@@ -148,29 +148,29 @@ const Contact = () => {
     
 
   </div>
-  <div class="container px-5 py-16 mx-auto flex">
-    <div class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
-      <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">GET FREE QUOTE </h2>
-      <p class="leading-relaxed mb-5 text-gray-600">Contact our team for questions, feedback, or support</p>
+  <div className="container px-5 py-16 mx-auto flex">
+    <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
+      <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">GET FREE QUOTE </h2>
+      <p className="leading-relaxed mb-5 text-gray-600">Contact our team for questions, feedback, or support</p>
       
-      <div class="relative mb-4">
-        <label for="text" class="leading-7 text-sm text-gray-600">Name</label>
-        <input type="text" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+      <div className="relative mb-4">
+        <label for="text" className="leading-7 text-sm text-gray-600">Name</label>
+        <input type="text" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
       </div>
-      <div class="relative mb-4">
-        <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
-        <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+      <div className="relative mb-4">
+        <label for="email" className="leading-7 text-sm text-gray-600">Email</label>
+        <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
       </div>
-      <div class="relative mb-4">
-        <label for="text" class="leading-7 text-sm text-gray-600">Subject</label>
-        <input type="text" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+      <div className="relative mb-4">
+        <label for="text" className="leading-7 text-sm text-gray-600">Subject</label>
+        <input type="text" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
       </div>
-      <div class="relative mb-4">
-        <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
-        <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+      <div className="relative mb-4">
+        <label for="message" className="leading-7 text-sm text-gray-600">Message</label>
+        <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
       </div>
-      <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-      <p class="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
+      <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+      <p className="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
     </div>
   </div>
 </section>
--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -12,15 +12,15 @@ const Explore = () => (
 <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="flex flex-col mb-6 lg:justify-between lg:flex-row md:mb-8">
        
-<div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+<div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
     <div>
-      <p class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+      <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
         Brand new
       </p>
     </div>
-    <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-      <span class="relative inline-block">
-        <svg viewBox="0 0 52 24" fill="currentColor" class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
+    <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+      <span className="relative inline-block">
+        <svg viewBox="0 0 52 24" fill="currentColor" className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
           <defs>
             <pattern id="7b568941-9ed0-4f49-85a0-5e21ca6c7ad6" x="0" y="0" width=".135" height=".30">
               <circle cx="1" cy="1" r=".7"></circle>
@@ -31,7 +31,7 @@ const Explore = () => (
       </span>
       OUR WORK / PORTFOLIO
     </h2>
-    <p class="text-base text-gray-700 md:text-lg">
+    <p className="text-base text-gray-700 md:text-lg">
       Check out some of our latest projects and see how we've helped our clients achieve their business goals.
     </p>
   </div>
